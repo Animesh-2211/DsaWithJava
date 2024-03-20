b6baf36f4381c25dc116f4ee7b5f638a72ceec9f
Animesh-2211 <reachanimeshhere@gmail.com>
2024-03-21 01:21:04 +0530

    Added some questions in  easy array part

---
+++ Arrays/Easy/missingnumbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+
+        int sum1 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 = sum1 + nums[i];
+        }
+
+        int missing = sum - sum1;
+        return missing;
+    }
+}
